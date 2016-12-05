59997aa5b22a2be8c985aa9490fbee612e96c222
Padraig Gleeson <p.gleeson@gmail.com>
2016-12-05 16:32:01 +0000

    Regenrated and saving spike times

--- PyNN/NeuroMLTest_PyNN0.8.py
+++ PyNN/NeuroMLTest_PyNN0.8.py
@@ -11,6 +11,7 @@ from importlib import import_module
 import numpy as np
 
 from pyNN.utility import get_script_args
+from neo.io import PyNNTextIO
 
 simulator_name = get_script_args(1)[0]  
 sim = import_module("pyNN.%s" % simulator_name)
@@ -23,6 +24,7 @@ sim.setup(timestep=time_step, debug=True)
 cell_params1 = {'tau_refrac':10,'v_thresh':-52.0, 'v_reset':-62.0, 'i_offset': 0.9, 'tau_syn_E'  : 2.0, 'tau_syn_I': 5.0}
 pop_IF_curr_alpha = sim.Population(1, sim.IF_curr_alpha(**cell_params1), label="pop_IF_curr_alpha")
 pop_IF_curr_alpha.record('v')
+pop_IF_curr_alpha.record('spikes')
 #pop_IF_curr_alpha.initialize('v', -67)
 
 cell_params2 = {'tau_refrac':8,'v_thresh':-50.0, 'v_reset':-70.0, 'i_offset': 1, 'tau_syn_E'  : 2.0, 'tau_syn_I': 5.0}
@@ -38,17 +40,20 @@ pop_IF_cond_alpha.record('v')
 cell_params4 = {'tau_refrac':5,'v_thresh':-52.0, 'v_reset':-68.0, 'i_offset': 1, 'tau_syn_E'  : 2.0, 'tau_syn_I': 5.0}
 pop_IF_cond_exp = sim.Population(1, sim.IF_cond_exp(**cell_params4), label="pop_IF_cond_exp")
 pop_IF_cond_exp.record('v')
+pop_IF_cond_exp.record('spikes')
 #pop_IF_cond_exp.initialize('v', -70)
 
 ##TODO: Test a>0!!
 cell_params5 = {'tau_refrac':0,'v_thresh':-52.0, 'v_reset':-68.0, 'i_offset': 0.6, 'v_spike': -40, 'a': 0.0, 'b':0.0805}
 pop_EIF_cond_exp_isfa_ista = sim.Population(1, sim.EIF_cond_exp_isfa_ista(**cell_params5), label="pop_EIF_cond_exp_isfa_ista")
 pop_EIF_cond_exp_isfa_ista.record('v')
+pop_EIF_cond_exp_isfa_ista.record('spikes')
 
 
 cell_params6 = {'i_offset': 0.2, 'gbar_K':6.0, 'gbar_Na':20.0}
 pop_HH_cond_exp = sim.Population(1, sim.HH_cond_exp(**cell_params6), label="pop_HH_cond_exp")
 pop_HH_cond_exp.record('v')
+#pop_HH_cond_exp.record('spikes')
 
 # Post synaptic cells
 cell_params_post1 = {'tau_refrac':10,'v_thresh':-52.0, 'v_reset':-62.0, 'i_offset': 0, 'tau_syn_E'  : 5.0}
@@ -93,11 +98,18 @@ else:
         data =  pop.get_data('v', gather=False)
         filename = "%s_v.dat"%(pop.label)
         print("Writing data for %s"%pop)
-        for segment in data.segments :
+        for segment in data.segments:
             vm = segment.analogsignalarrays[0].transpose()[0]
             tt = np.array([t*time_step/1000. for t in range(len(vm))])
             times_vm = np.array([tt, vm/1000.]).transpose()
             np.savetxt(filename, times_vm , delimiter = '\t', fmt='%s')
+        filename = "%s.spikes"%(pop.label)
+        io = PyNNTextIO(filename=filename)
+        spikes =  pop.get_data('spikes', gather=False)
+        if len(spikes.segments[0].spiketrains)>0:
+            print("Writing data for %s"%pop)
+            for segment in spikes.segments:
+                io.write_segment(segment)
 
 
 sim.end()
@@ -142,6 +154,9 @@ if '-gui' in sys.argv:
 
 
         plt.show()
+        
+else:
 
+    print("Simulation completed.\nRun this script with flag: -gui to plot activity of a number of cells")
 
 
